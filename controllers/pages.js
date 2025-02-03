8ec12fe8b5fa1fb6dfc2e87ca509050998b22278
mahmood <mahmoodalmajed@gmail.com>
2025-02-03 15:58:16 +0300

    templete

---
+++ controllers/pages.js
@@ -0,0 +1,12 @@
+const home =(req,res)=>{
+
+    res.render('index.ejs' ,{title: 'My App'});
+};
+
+
+
+
+module.exports={
+
+    home,
+}
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app=express();
 const mongoose=require('mongoose');
 const methodOverride=require('method-override');
 const morgan =require('morgan');
+const path =require('path');//for css
 
 const port = process.env.PORT ? process.env.PORT:'3000';
 
@@ -16,6 +17,20 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected',()=>{
 console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 })
+
+//MIDDLEWARE 
+
+app.use(express.static(path.join(__dirname, "public")));//for css styling 
+
+
+
+//CONTROLLERS
+const pagesCtrl=require('./controllers/pages')
+
+
+
+//ROUTE HANDELERS
+app.get('/',pagesCtrl.home)
 
 
 app.listen(port,()=>{
